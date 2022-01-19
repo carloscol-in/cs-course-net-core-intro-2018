ec7ac1e738790295ae526d5d571d725dd6a83bcd
Carlos C <contact@carloscolin.me>
2022-01-18 23:27:41 -0600

    Changed DrawLine method name

--- Util/Printer.cs
+++ Util/Printer.cs
@@ -4,20 +4,20 @@ namespace CoreEscuela.Util
 {
     public static class Printer
     {
-        public static void DibujarLinea(int tam = 10)
+        public static void DrawLine(int tam = 10)
         {
             WriteLine("".PadLeft(tam, '='));
         }
 
         public static void WriteTitle(string titulo)
         {
-            var tamaño =titulo.Length + 4;
-            DibujarLinea(tamaño);
+            var tamaño = titulo.Length + 4;
+            DrawLine(tamaño);
             WriteLine($"| {titulo} |");
-            DibujarLinea(tamaño);
+            DrawLine(tamaño);
         }
 
-        public static void Beep(int hz = 2000, int tiempo=500, int cantidad =1)
+        public static void Beep(int hz = 2000, int tiempo = 500, int cantidad = 1)
         {
             while (cantidad-- > 0)
             {
